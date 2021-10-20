aa4b8155ba01c3154bb10a1d37a15cd56c905ead
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-20 15:32:58 -0400

    finish the spec

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # infinite-scroll-quotes
 > Example testing the infinite scroll using Cypress
 
-Example page copied from [JavaScript Infinite Scroll](https://www.javascripttutorial.net/javascript-dom/javascript-infinite-scroll/)
+Watch the video [Testing The Infinite Scroll With Cypress](https://youtu.be/918BqmTa-w4)
+
+Tests in [cypress/integration/spec.js](./cypress/integration/spec.js)
+
+## Install and run
+
+```
+$ npm install
+$ npm run dev
+```
+
+Start the application and opens Cypress. Click on the spec to run all tests
+
+The example page copied from [JavaScript Infinite Scroll](https://www.javascripttutorial.net/javascript-dom/javascript-infinite-scroll/)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,5 +1,64 @@
 /// <reference types="cypress" />
 
-it('fetches items on scroll', () => {
+it('shows loading element on scroll', () => {
   cy.visit('/')
+  cy.get('.loader').should('be.visible')
+  cy.get('.loader').should('not.be.visible')
+
+  cy.window().scrollTo('bottom')
+  cy.get('.loader').should('be.visible')
+  cy.get('.loader').should('not.be.visible')
+
+  cy.window().scrollTo('bottom')
+  cy.get('.loader').should('be.visible')
+  cy.get('.loader').should('not.be.visible')
+})
+
+it('fetches N items at a time', () => {
+  cy.visit('/')
+  cy.get('.quote').should('have.length.greaterThan', 5)
+  .then(quotes => {
+    cy.window().scrollTo('bottom')
+    cy.get('.quote').should('have.length', quotes.length * 2)
+
+    cy.window().scrollTo('bottom')
+    cy.get('.quote').should('have.length', quotes.length * 3)
+
+    cy.window().scrollTo('bottom')
+    cy.get('.quote').should('have.length', quotes.length * 4)
+  })
+})
+
+it('fetches quotes from API', () => {
+  cy.intercept('GET', '**/v1/quotes/*').as('quotes')
+  cy.visit('/')
+  cy.wait('@quotes')
+  cy.get('.quote').should('have.length', 10)
+
+  cy.window().scrollTo('bottom')
+  cy.wait('@quotes')
+  cy.get('.quote').should('have.length', 20)
+
+  cy.window().scrollTo('bottom')
+  cy.wait('@quotes')
+  cy.get('.quote').should('have.length', 30)
+
+  cy.window().scrollTo('bottom')
+  cy.wait('@quotes')
+  cy.get('.quote').should('have.length', 40)
+})
+
+it('goes back to the top', () => {
+  cy.visit('/')
+  cy.window().its('scrollY').should('equal', 0)
+  cy.window().scrollTo('bottom')
+  cy.get('.quote').should('have.length', 20)
+  cy.window().its('scrollY').should('be.greaterThan', 100)
+
+  cy.window().scrollTo('bottom')
+  cy.get('.quote').should('have.length', 30)
+  cy.window().its('scrollY').should('be.greaterThan', 500)
+
+  cy.get('#back-to-top').click()
+  cy.window().its('scrollY').should('equal', 0)
 })
